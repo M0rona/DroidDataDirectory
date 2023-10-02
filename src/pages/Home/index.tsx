5f9f8b6c38eedbae0093b975092858df9dfa5ed6
M0rona <gabrielmorona0229@gmail.com>
2023-10-02 01:45:27 -0300

    Correção mobile

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,72 @@
+import { useEffect } from 'react';
+import { useCharacterContext } from '../../contexts/Characters';
+
+import LogoStarWars from '../../assets/logos/StarWars.png'
+import LogoInnova from '../../assets/logos/Innova.png'
+import iconBack from '../../assets/icons/iconBack.svg'
+
+import { SectionCharacters } from './SectionCharacters'
+import { SectionInfos } from './SectionInfos'
+
+import * as Styled from './styles'
+import GlobalStyles from '../../styles/Global';
+
+export const Home = () => {
+  const { 
+    windowWidth, 
+    isMobile, 
+    setIsMobile, 
+    getCharacters } = useCharacterContext();
+
+  const returnCharacters = () => {
+    if(windowWidth < 1024 && isMobile) setIsMobile(false)
+  }
+
+  useEffect(() => {
+    getCharacters(true);
+  }, [])
+
+  return (
+    <>
+        
+        <Styled.Header>
+          <img src={LogoStarWars} alt="LogoStarWars" />
+          <img src={LogoInnova} alt="LogoInnova" />
+        </Styled.Header>
+
+        {windowWidth < 1024 && isMobile  && 
+          <Styled.ButtonBack onClick={returnCharacters}>
+            <img src={iconBack} alt="iconBack" />
+            <span>Return to characters</span>
+          </Styled.ButtonBack>
+        }
+
+        <Styled.Main>
+            { (windowWidth > 1024 ? true : !isMobile) &&
+              <Styled.Section 
+                $width={windowWidth < 1024 ? "100%" : "45rem"} 
+                $transp={true} 
+                $leaked={true}
+                $scroll={windowWidth > 1024 ? true : !isMobile}
+              >
+                  <SectionCharacters />
+              </Styled.Section>
+            }
+
+            { (windowWidth > 1024 ? true : isMobile)  && 
+              <Styled.Section 
+                $width={windowWidth < 1024 ? "100%" : "100rem"} 
+                $padding={true} 
+                $gap={true}
+                $scroll={windowWidth > 1024}
+              >
+                <SectionInfos />
+              </Styled.Section>
+            }
+            
+        </Styled.Main>
+
+        <GlobalStyles $scrollPage={windowWidth > 1024 ? true : !isMobile} />
+    </>
+  )
+}
